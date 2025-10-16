a91f60dec0338e2089bfa867dff0a85de652503b
Sunny <sunnykumarrasulla@gmail.com>
2025-10-16 22:35:15 +0530

    a

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Routes, Navigate, Route } from "react-router-dom";
 import Home from "./Home/Home";
-import Courses from "./Courses/Courses";
+import Courses from "./courses/Courses";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import { Toaster } from "react-hot-toast";
